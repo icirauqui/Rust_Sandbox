bf6b94b0d665f323c2aa167f938c489e73e2c56b
icirauqui <icirauqui@gmail.com>
2022-06-30 21:29:57 +0200

    Update NN

--- src/sql.rs
+++ src/sql.rs
@@ -94,3 +94,101 @@ fn main() {
 */
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn main() -> Result<(), Error> {
+
+    let url = "postgresql://postgres:postgres@localhost:5432/postgres";
+    let mut conn = Client::connect(url, NoTls).unwrap();
+
+    let mut sn : VecVec64 = vec![];
+
+
+    for row in conn.query("SELECT * from sn", &[])? {
+
+        // let var00 : Option<f64> = row.get(0);
+        // let var01 : Option<f64> = row.get(1);
+        // let var02 : Option<f64> = row.get(2);
+        // let var03 : Option<f64> = row.get(3);
+        // let var04 : Option<f64> = row.get(4);
+        // let var05 : Option<f64> = row.get(5);
+        // let var06 : Option<f64> = row.get(6);
+        // let var07 : Option<f64> = row.get(7);
+        // let var08 : Option<f64> = row.get(8);
+        // let var09 : Option<f64> = row.get(9);
+        // let var10 : Option<f64> = row.get(10);
+        // let var11 : Option<f64> = row.get(11);
+        // let var12 : Option<f64> = row.get(12);
+        // let var13 : Option<f64> = row.get(13);
+        // let var14 : Option<f64> = row.get(14);
+        // let var15 : Option<f64> = row.get(15);
+        // let var16 : Option<f64> = row.get(16);
+        // let var17 : Option<f64> = row.get(17);
+        // let var18 : Option<f64> = row.get(18);
+        // let var19 : Option<f64> = row.get(19);
+        // let var20 : Option<f64> = row.get(20);
+
+
+        sn.push(vec![row.get(0),
+                     row.get(1),
+                     row.get(2),
+                     row.get(3),
+                     row.get(4),
+                     row.get(5),
+                     row.get(6),
+                     row.get(7),
+                     row.get(8),
+                     row.get(9),
+                     row.get(10),
+                     row.get(11),
+                     row.get(12),
+                     row.get(13),
+                     row.get(14),
+                     row.get(15),
+                     row.get(16),
+                     row.get(17),
+                     row.get(18),
+                     row.get(19),
+                     row.get(20)]);
+
+        //println!(
+        //    "row i : {}) {}",
+        //    var00.unwrap(), var01.unwrap()
+        //);
+    }
+
+    println!("{}", sn[0][0].unwrap());
+
+
+
+    // Split dataset into train and test
+    let mut x_train_1 : VecVec64 = vec![];
+    let mut y_train_1 : Vec<Option<f64>> = vec![];
+    let mut x_test_1 : VecVec64 = vec![];
+    let mut y_test_1 : Vec<Option<f64>> = vec![];
+
+    let mut i : i64 = 0;
+    let split : i64 = (sn.len() as f64 * 0.9) as i64;
+    for n in sn {
+        if i < split {
+            x_train_1.push(n[0..20].to_vec());
+            y_train_1.push(n[20]);
+        } else {
+            x_test_1.push(n[0..20].to_vec());
+            y_test_1.push(n[20]);
+        }
+        i = i + 1;
+    }
+
+
